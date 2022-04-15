c9d107b9cff5719f6b13e8de036bd03a7ee8721d
bbkincso <bbkincso@gmail.com>
2022-04-15 20:27:18 +0100

    working on cloud config

---
+++ cloud-gateway/src/main/java/com/library/cloudgateway/filters/LoggingPostFilter.java
@@ -0,0 +1,42 @@
+package com.library.cloudgateway.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class LoggingPostFilter extends ZuulFilter {
+
+    Logger log = LoggerFactory.getLogger(LoggingPostFilter.class);
+
+    @Override
+    public String filterType() {
+        return "post";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        HttpServletResponse response = ctx.getResponse();
+
+        log.info(String.format("POST: %s status from %s ", response.getStatus(), request.getRequestURL().toString()));
+        return null;
+    }
+}
---
+++ cloud-gateway/src/main/java/com/library/cloudgateway/filters/LoggingPreFilter.java
@@ -0,0 +1,39 @@
+package com.library.cloudgateway.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class LoggingPreFilter extends ZuulFilter {
+    Logger log = LoggerFactory.getLogger(LoggingPreFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        log.info(String.format("PRE: %s request to %s ", request.getMethod(), request.getRequestURL().toString()));
+        return null;
+    }
+}
